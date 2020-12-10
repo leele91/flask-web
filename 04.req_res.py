271ffaba15ae074ed61a15611e30d3edfd0ab17a
lee le <wlsduddl013@gmail.com>
2020-12-10 12:19:00 +0900

    first commit

---
+++ 04.req_res.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from flask import Response, make_response
+app = Flask(__name__)
+
+@app.route('/area')
+def area():
+    pi = request.args.git('pi', '3.14') # pi 값이 주어지지 않으면 3.14를 주어라
+    radius = request.args['radius']
+    s = float(pi) * float(radius)
+    return f'pi={pi}, radius ={radius}, area={s}'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('03.login.htm')
+    else:
+        uid = request.form['uid']
+        pwd = request.form['pwd']
+        return f'uid={uid}, pwd={pwd}'
+
+@app.route('/response')
+def response_fn():
+    custom_res = Response('Custom Response', 200, {'test': 'ttt'})
+    return make_response(custom_res)
+
+if __name__ == '__main__':
+    app.run(debug=True)
