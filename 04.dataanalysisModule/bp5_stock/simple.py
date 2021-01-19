8e02bd5edc7589cd741e8d5ef747ff2b22ba7e08
lee le <wlsduddl013@gmail.com>
2021-01-19 10:39:50 +0900

    카토그램

--- 04.dataanalysisModule/bp5_stock/simple.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-#simple_bp = Blueprint('simple_bp', __name__, template_folder='templates') #개별 폴더
-simple_bp = Blueprint('simple_bp', __name__) # 원래의 templates 위치에서
-
-@simple_bp.route('/')
-def simple():
-    return render_template('stock/simple.html')
