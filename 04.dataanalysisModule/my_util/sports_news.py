4f229ce861dbff2e9f8b7054ed364a48b5b4f0fb
lee le <wlsduddl013@gmail.com>
2021-01-19 10:40:36 +0900

    스포츠뉴스

--- 04.dataanalysisModule/my_util/sports_news.py
+++ 04.dataanalysisModule/my_util/sports_news.py
@@ -2,7 +2,7 @@ import requests, time
 from selenium import webdriver
 
 def get_sports_news(filename):
-    driver = webdriver.Chrome('./bp6_wordcloud/chromedriver')
+    driver = webdriver.Chrome('./bp5_wordcloud/chromedriver')
     url = 'https://sports.news.naver.com/index.nhn'
     driver.get(url)
     time.sleep(1)
