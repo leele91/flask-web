4485115f7f411767ed2d3fbfb501cff54b0ac03f
lee le <wlsduddl013@gmail.com>
2021-01-29 17:31:13 +0900

    회귀

--- 04.dataanalysisModule/bp6_classification/clsf.py
+++ 04.dataanalysisModule/bp6_classification/clsf.py
@@ -31,7 +31,7 @@ def titanic():
     if request.method == 'GET':
             return render_template('classification/titanic.html', menu=menu, weather=get_weather())
     else:
-        index = int(request.form['index'])
+        index = int(request.form['index'] or '0')
         df = pd.read_csv('static/data/titanic_test.csv')
         scaler = joblib.load('static/model/titanic_scaler.pkl')
         test_data = df.iloc[index, 1:].values.reshape(1,-1)
@@ -69,7 +69,7 @@ def pima():
     if request.method == 'GET':
             return render_template('classification/pima.html', menu=menu, weather=get_weather())
     else:
-        index = int(request.form['index'])
+        index = int(request.form['index'] or '0')
         df = pd.read_csv('static/data/pima_test.csv')
         scaler = joblib.load('static/model/pima_scaler.pkl')
         test_data = df.iloc[index, :-1].values.reshape(1,-1)
@@ -97,7 +97,7 @@ def cancer():
     if request.method == 'GET':
             return render_template('classification/cancer.html', menu=menu, weather=get_weather())
     else:
-        index = int(request.form['index'])
+        index = int(request.form['index'] or '0')
         df = pd.read_csv('static/data/cancer_test.csv')
         scaler = MinMaxScaler()
         scaled_test = scaler.fit_transform(df.iloc[:, :-1])
@@ -126,7 +126,7 @@ def iris():
             return render_template('classification/iris.html', menu=menu, weather=get_weather())
     else:
         # pass
-        index = int(request.form['index'])
+        index = int(request.form['index'] or '0')
         df = pd.read_csv('static/data/iris_test.csv')
         scaler = joblib.load('static/model/iris_scaler.pkl')
         test_data = df.iloc[index, :-1].values.reshape(1,-1)
@@ -159,7 +159,7 @@ def wine():
             return render_template('classification/wine.html', menu=menu, weather=get_weather())
     else:
         # pass
-        index = int(request.form['index'])
+        index = int(request.form['index'] or '0')
         df = pd.read_csv('static/data/wine_test.csv')
         scaler = joblib.load('static/model/wine_scaler.pkl')
         test_data = df.iloc[index, :-1].values.reshape(1,-1)
