3a75abf49c7e857f0c49673769003b163812d1ff
lee le <wlsduddl013@gmail.com>
2021-02-02 14:21:49 +0900

    군집화

--- 04.dataanalysisModule/bp9_clustering/clus.py
+++ 04.dataanalysisModule/bp9_clustering/clus.py
@@ -30,7 +30,7 @@ def get_weather_main():
 def cluster():
     menu = {'ho':0, 'da':0, 'ml':1, 
             'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':0,
-            'cf':0, 'ac':0, 're':0, 'cu':1, 'st':0, 'nl':1}
+            'cf':0, 'ac':0, 're':0, 'cu':1, 'st':0, 'nl':0}
 
     if request.method == 'GET':
             return render_template('cluster/cluster.html', menu=menu, weather=get_weather())
