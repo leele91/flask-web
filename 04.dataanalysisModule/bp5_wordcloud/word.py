3843cf40875b218fd0be612af7e1fb795562036f
lee le <wlsduddl013@gmail.com>
2021-02-02 14:09:10 +0900

    워드 클라우드

--- 04.dataanalysisModule/bp5_wordcloud/word.py
+++ 04.dataanalysisModule/bp5_wordcloud/word.py
@@ -9,6 +9,9 @@ from my_util.wordcloud import engCloud, hanCloud
 from my_util.sports_news import get_sports_news
 
 word_bp = Blueprint('word_bp', __name__)
+menu = {'ho':0, 'da':1, 'ml':0, 
+            'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':1,
+            'cf':0, 'ac':0, 're':0, 'cu':0, 'st':0, 'nl':0}
 
 def get_weather_main():
     weather = None
@@ -24,9 +27,6 @@ def get_weather_main():
 
 @word_bp.route('/word', methods=['GET', 'POST'])
 def word():
-    menu = {'ho':0, 'da':1, 'ml':0, 
-            'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':1,
-            'cf':0, 'ac':0, 're':0, 'cu':0, 'st':0}
     if request.method == 'GET':
         return render_template('wordcloud/word.html', menu=menu, weather=get_weather_main())
     else:
@@ -57,9 +57,6 @@ def word():
 
 @word_bp.route('/sports_news')
 def sports_news():
-    menu = {'ho':0, 'da':1, 'ml':0, 
-            'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':1,
-            'cf':0, 'ac':0, 're':0, 'cu':0, 'st':0}
     text_file = os.path.join(current_app.root_path, 'static/data/sports.txt')
     get_sports_news(text_file)
 
